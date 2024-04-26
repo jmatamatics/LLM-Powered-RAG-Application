c89963d216b8179f79491fd51233558c7eac2927
“Joseph <joseph.mata@flatironschool.com>
2024-04-26 14:01:30 -0400

    update

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if prompt := st.chat_input("What's your question?"):
                 source = rel_docs[i].metadata['source'][5:] # take out folder name from string
                 page = rel_docs[i].metadata['page'] +1 #counts starting at zero
                 st.write(f" :book: **page {page} of** ***{source}***") 
-                st.write('**Relevant Text/Chunk:**',rel_docs[i].page_content)
+                #st.write('**Relevant Text/Chunk:**',rel_docs[i].page_content)
                 pdf_viewer(rel_docs[i].metadata['source'],width = 670, height =790, pages_to_render =[page], key=f"pdf_viewer_{i+100}")
 
                 st.write("--------------------------------")
